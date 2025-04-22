3062bad1e86c73662dbabbcb21ed7cf7f6c92717
kyle <miller.kwil@gmail.com>
2025-04-21 22:21:18 -0700

    initial commit

---
+++ lua/kmill/maps.lua
@@ -0,0 +1,30 @@
+vim.g.mapleader = " "
+-- Define a helper function for creating keymaps with silent option enabled
+local function map(mode, lhs, rhs)
+	vim.keymap.set(mode, lhs, rhs, { silent = true })
+end
+
+map("n", "<leader>l", "<CMD>Lazy<CR>") -- Save the current buffer (file)
+
+map("n", "<leader>w", "<CMD>update<CR>") -- Save the current buffer (file)
+
+map("n", "<leader>q", "<CMD>q<CR>") -- Quit Neovim
+
+map("i", "jk", "<ESC>") -- Exit insert mode
+
+map("n", "<leader>e", "<CMD>Neotree toggle<CR>") -- Toggle file explorer sidebar
+map("n", "<leader>r", "<CMD>Neotree focus<CR>") -- Focus the file explorer (if already open)
+
+-- Window Mgmt
+map("n", "<leader>o", "<CMD>vsplit<CR>") -- Create vertical split
+map("n", "<leader>p", "<CMD>split<CR>") -- Create horizontal split
+
+map("n", "<C-h>", "<C-w>h") -- Move to the window on the left
+map("n", "<C-l>", "<C-w>l") -- Move to the window on the right
+map("n", "<C-k>", "<C-w>k") -- Move to the window above
+map("n", "<C-j>", "<C-w>j") -- Move to the window below
+
+map("n", "<C-Left>", "<C-w><") -- Decrease window width
+map("n", "<C-Right>", "<C-w>>") -- Increase window width
+map("n", "<C-Up>", "<C-w>+") -- Increase window height
+map("n", "<C-Down>", "<C-w>-") -- Decrease window height
---
+++ lua/kmill/plugins/mason.lua
@@ -0,0 +1,41 @@
+return {
+	"williamboman/mason.nvim",
+	dependencies = {
+		"williamboman/mason-lspconfig.nvim",
+		"WhoIsSethDaniel/mason-tool-installer.nvim",
+	},
+	config = function()
+		require("mason").setup()
+
+		require("mason-lspconfig").setup({
+			automatic_installation = true,
+			ensure_installed = {
+				"cssls",
+				"eslint",
+				"html",
+				"lau_la",
+				"json_ls",
+				"tsserver",
+				"pyright",
+				"tailwindcss",
+			},
+		})
+
+		require("mason-tool-installer").setup({
+			ensure_installed = {
+				"prettier",
+				"stylua", -- lua formatter
+				"isort", -- python formatter
+				"black", -- python formatter
+				"pylint",
+				"eslint_d",
+			},
+		})
+	end,
+	opts = {
+		ensure_installed = {
+			"stylua",
+			"shfmt",
+		},
+	},
+}
